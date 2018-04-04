24caacf3e594f9bdbad5395f3cd622a4c11f36a9
ec0tests <sergiobcn1996@gmail.com>
2018-04-05 01:44:45 +0200

    kk

--- src/App.js
+++ src/App.js
@@ -6,35 +6,42 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header>
-                    {/*   MENUS SUPER */}
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-6 text-left">
-                                <img src="https://via.placeholder.com/383x80" className="img-fluid" alt=""/>
-                            </div>
-                            <div className="col-3 text-left">
-                                <button className="Megamenu">Vuela en globo</button>
+                <section id="Section1" className="Section1">
+
+                    <header>
+                        {/*   MENUS SUPER */}
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-6 text-left">
+                                    <img src="https://via.placeholder.com/383x80" className="img-fluid" alt=""/>
+                                </div>
+                                <div className="col-6 d-flex align-items-center justify-content-end pr-5">
+                                    <button className="Megamenu">Vuela en globo</button>
+                                    <span className="ml-4">MENU</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-
+                    </header>
+                    {/*
+                ---------
+                SECCION 1
+                ---------*/}
                     {/*   Principal*/}
-                    <div className="container">
-                            <div className="col-12 text-left">
-                                <h1>VUELA EN GLOBO <br/> <span>en el parque natural de La Garrotxa</span></h1>
-                            </div>
-                            <div className="col-12 text-left">
-                                <button className="text-uppercase">Reservar</button>
-                            </div>
-                            <div className="col-12 text-left">
-                                <button className="text-uppercase">Canjear cupón</button>
-                            </div>
-                            <div className="col-12 text-left">
-                                <button className="text-uppercase">regalar vuelo</button>
-                            </div>
+                    <div className="container pb-5 mb-2">
+                        <div className="col-12 text-left">
+                            <h1>VUELA EN GLOBO <br/> <span>en el parque natural de La Garrotxa</span></h1>
+                        </div>
+                        <div className="col-12 text-left ">
+                            <button className="text-uppercase Btn-red-h mt-2">Reservar</button>
+                        </div>
+                        <div className="col-12 text-left ">
+                            <button className="text-uppercase Btn-darkblue-h mt-2">Canjear cupón</button>
+                        </div>
+                        <div className="col-12 text-left">
+                            <button className="text-uppercase Btn-darkblue-h mt-2">regalar vuelo</button>
+                        </div>
                     </div>
-                    <div className="container">
+                    <div className="container pt-5">
                         <div className="row ">
                             <div className="Iconos-header col-6 p-0 d-flex">
                                 <div className="col-4">
@@ -65,19 +72,223 @@ class App extends Component {
                                 </div>
                             </div>
                             <div className="col-12 mt-5 text-left">
-                               <p>* Vuelos adaptados & 100% accesibles <br/> Personas mayores , discapacidades, sillas de ruedas </p>
+                                <p>* Vuelos adaptados & 100% accesibles <br/> Personas mayores , discapacidades, sillas
+                                    de ruedas </p>
                             </div>
                         </div>
 
                     </div>
 
+                </section>
+                <section id="Section2" className="Section2">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-md-4">
+                                <div className="card w-100">
+                                    <div className="card-body text-left">
+                                        <h5 className="card-title">Card title</h5>
+                                        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                        <p className="card-text">Some quick example text to build on the card title and
+                                            make up the bulk of the card's content.</p>
+                                        <a href="#" className="card-link">Card link</a>
+                                        <a href="#" className="card-link">Another link</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">VolColoms</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                        <div className="row justify-content-center">
+                            <div className="col-1">
+                                <img src="https://via.placeholder.com/80x80" alt=""/>
+                            </div>
+                            <div className="col-1">
+                                <img src="https://via.placeholder.com/80x80" alt=""/>
+                            </div>
+                            <div className="col-1">
+                                <img src="https://via.placeholder.com/80x80" alt=""/>
+                            </div>
+                            <div className="col-12">
+                                <h2>Personaliza tu experiencia</h2>
+                                <h3>Combina tu vuelo con alojamiento, comida o actividades turísticas.</h3>
+                                <button className="Btn-blue">Vuela en globo</button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="Section3" className="Section3 py-5">
+                    <div className="container">
+                        <div className="row ">
+                            <div className="col-md-6 text-left">
+                                <h5>25 AÑOS VOLANDO</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad adipisci amet
+                                    dignissimos doloremque exercitationem fuga incidunt minus natus necessitatibus
+                                    nostrum officia placeat praesentium quaerat recusandae, reprehenderit rerum ullam
+                                    unde?</p>
+                                <br/>
+                                <h5>25 AÑOS VOLANDO</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad adipisci amet
+                                    dignissimos doloremque exercitationem fuga incidunt minus natus necessitatibus
+                                    nostrum officia placeat praesentium quaerat recusandae, reprehenderit rerum ullam
+                                    unde?</p>
+                                <br/>
+                                <h5>25 AÑOS VOLANDO</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad adipisci amet
+                                    dignissimos doloremque exercitationem fuga incidunt minus natus necessitatibus
+                                    nostrum officia placeat praesentium quaerat recusandae, reprehenderit rerum ullam
+                                    unde?</p>
+                                <br/>
+                                <h5>25 AÑOS VOLANDO</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad adipisci amet
+                                    dignissimos doloremque exercitationem fuga incidunt minus natus necessitatibus
+                                    nostrum officia placeat praesentium quaerat recusandae, reprehenderit rerum ullam
+                                    unde?</p>
+                                <button className="Btn-darkblue">Más info</button>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </section>
+
+
+                <section id="Section4" className="Section4">
+                    <div className="container">
+                        <h2 className="text-uppercase">La experiencia completa </h2>
+                        <h3>Una oferta para cada gusto</h3>
+                        <div className="row justify-content-center">
+
+                            <div className="col-md-3 ">
+                                <h5>Aventura</h5>
+
+                                <img src="https://via.placeholder.com/320x250" className="img-fluid" alt=""/>
+
+
+                            </div>
+                            <div className="col-md-3 ">
+                                <h5>Aventura</h5>
+
+                                <img src="https://via.placeholder.com/320x250" className="img-fluid" alt=""/>
+
+
+
+                            </div>
+                            <div className="col-md-3 ">
+                                <h5>Aventura</h5>
+
+                                <img src="https://via.placeholder.com/320x250" className="img-fluid" alt=""/>
+
+
+                            </div>
+                            <div className="col-md-3 ">
+                                <h5>Aventura</h5>
+
+                                <img src="https://via.placeholder.com/320x250" className="img-fluid" alt=""/>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </section>
+
+
+
+                <section id="Section5" className="Section5">
+                    <div className="container-fluid">
+                        <h2 className="text-uppercase mb-3">Sube tu foto y gana un vuelo con #voldecoloms </h2>
+                        <div className="row justify-content-center">
+                            <div className="col-md-2 ">
+                                <img src="https://via.placeholder.com/320x250" className="img-fluid" alt=""/>
+                            </div>
+                            <div className="col-md-2 ">
+                                <img src="https://via.placeholder.com/320x250" className="img-fluid" alt=""/>
+                            </div>
+                            <div className="col-md-2 ">
+                                <img src="https://via.placeholder.com/320x250" className="img-fluid" alt=""/>
+                            </div>
+                            <div className="col-md-2 ">
+                                <img src="https://via.placeholder.com/320x250" className="img-fluid" alt=""/>
+                            </div>
+                            <div className="col-md-2 ">
+                                <img src="https://via.placeholder.com/320x250" className="img-fluid" alt=""/>
+                            </div>
+                            <div className="col-md-2 ">
+                                <img src="https://via.placeholder.com/320x250" className="img-fluid" alt=""/>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </section>
+
+                <footer className="">
+                    <div className="container-fluid">
+                        <div className="row Row1 p-3">
+                            <div className="col-lg-6 d-flex">
+                                <div className="col-lg-3 p-0 d-flex align-items-center">
+                                    <img className="img-fluid" src="https://via.placeholder.com/200x80" alt=""/>
+                                </div>
+                                <div className="col-lg-6 text-left d-flex align-items-center">
+                                   <span>VOL DE COLOMS</span>
+                                </div>
+
+                            </div>
+                            <div className="col-lg-6 d-flex flex-wrap pt-5">
+                                <div className="col-lg-5 p-0 d-flex align-items-center justify-content-end">
+                                    <button className="Btn-red">todos los vuelos</button>
+                                </div>
+                                <div className="col-lg-7 d-flex align-items-center">
+                                    <div className="col-lg-2 p-0">
+                                        <img src="https://via.placeholder.com/55x55" className="img-fluid" alt=""/>
+                                    </div>
+                                    <div className="col-lg-2 p-0">
+                                        <img src="https://via.placeholder.com/55x55" className="img-fluid" alt=""/>
+                                    </div>
+                                    <div className="col-lg-2 p-0">
+                                        <img src="https://via.placeholder.com/55x55" className="img-fluid" alt=""/>
+                                    </div>
+                                    <div className="col-lg-2 p-0">
+                                        <img src="https://via.placeholder.com/55x55" className="img-fluid" alt=""/>
+                                    </div>
+                                    <div className="col-lg-2 p-0">
+                                        <img src="https://via.placeholder.com/55x55" className="img-fluid" alt=""/>
+                                    </div>
+
+                                </div>
+                                <div className="col-lg-12 text-left mt-4 ">
+                                   <span className="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+                                </div>
+
+                            </div>
+
+                        </div>
+                        <div className="row Row2 p-3">
+                           <div className="Logos-patrocinadores">
+                               <img src="https://via.placeholder.com/320x80" className="img-fluid" alt=""/>
+                           </div>
+                            <div className="Logos-patrocinadores">
+                                <img src="https://via.placeholder.com/320x80" className="img-fluid" alt=""/>
+                            </div>
+                            <div className="Logos-patrocinadores">
+                                <img src="https://via.placeholder.com/320x80" className="img-fluid" alt=""/>
+                            </div>
+                            <div className="Logos-patrocinadores">
+                                <img src="https://via.placeholder.com/320x80" className="img-fluid" alt=""/>
+                            </div>
+                            <div className="Logos-patrocinadores">
+                                <img src="https://via.placeholder.com/320x80" className="img-fluid" alt=""/>
+                            </div>
+                            <div className="Logos-patrocinadores">
+                                <img src="https://via.placeholder.com/320x80" className="img-fluid" alt=""/>
+                            </div>
+                            <div className="Logos-patrocinadores">
+                                <img src="https://via.placeholder.com/320x80" className="img-fluid" alt=""/>
+                            </div>
+
+                        </div>
+                    </div>
+                </footer>
             </div>
         );
     }
