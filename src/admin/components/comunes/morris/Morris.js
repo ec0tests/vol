4c6f5f171be8ce2d8c7b13c7874fe1c22a515da7
ec0tests <sergiobcn1996@gmail.com>
2018-05-21 04:14:28 +0200

    d

--- src/admin/components/comunes/morris/Morris.js
+++ src/admin/components/comunes/morris/Morris.js
@@ -1,3 +1,4 @@
+/*
 import React, {Component} from 'react';
 import Raphael from 'raphael';
 import 'morris.js/morris.css';
@@ -24,3 +25,4 @@ class Morris extends React.Component {
 }
 
 export default Morris;
+*/
